fd1ef26127bd865fdb3df4a1edc9b7149b029b93
Robert Schultz <robert@cosmicrealms.com>
2019-03-10 20:53:38 -0400

    Renamed module to XU instead of xbase to avoid conflicts in the big scary world

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const base = require("./base.js"),
+const XU = require("./XU.js"),
 	performance= require("perf_hooks").performance;
 
 console.log("[%s]", "   \t\n\r \t this is   just a test  \t\t of the  system  \t  \n  \t\t\t ".trim().innerTrim());
@@ -9,7 +9,7 @@ console.log("[%s]", " this is   just a test  \t\t of the  system   \t  ".trim(["
 
 
 console.log("These numbers should start come out with 0.5 seconds between each number");
-[].pushSequence(1, 100).parallelForEach((v, subcb) => { console.log("%d: %d", performance.now(), v); setTimeout(subcb, Math.randomInt(0, base.SECOND*10)); }, () => {}, 10, base.SECOND/2);
+[].pushSequence(1, 100).parallelForEach((v, subcb) => { console.log("%d: %d", performance.now(), v); setTimeout(subcb, Math.randomInt(0, XU.SECOND*10)); }, () => {}, 10, XU.SECOND/2);
 
 
-//[].pushSequence(1, 100).serialForEach((v, subcb) => { console.log("%d: %d", performance.now(), v); setTimeout(subcb, base.SECOND/2); }, () => {});
+//[].pushSequence(1, 100).serialForEach((v, subcb) => { console.log("%d: %d", performance.now(), v); setTimeout(subcb, XU.SECOND/2); }, () => {});

28ac4cd1eff9300318c623642616d1aa4fe033a9
Robert Schultz <robert@cosmicrealms.com>
2021-07-20 13:49:30 -0400

    Added decodeURLPath

--- String.js
+++ String.js
@@ -218,7 +218,7 @@ if(!String.prototype.strip)
 {
 	String.prototype.strip = function strip(_chars)
 	{
-		const chars = Array.isArray(_chars) ? _chars.join() : Array.prototype.slice.call(arguments).join();	// eslint-disable-line prefer-rest-params
+		const chars = Array.isArray(_chars) ? _chars.join(",") : Array.prototype.slice.call(arguments).join(",");	// eslint-disable-line prefer-rest-params
 		return this.replace(new RegExp(`[${chars}]`, "g"), "");
 	};
 }
@@ -261,8 +261,17 @@ if(!String.prototype.encodeURLPath)
 		if(!skipEncodePercent)
 			r = r.replaceAll("%", "%25");
 		
-		r = r.replaceAll("#", "%23").replaceAll("?", "%3f").replaceAll("\r", "%0d").replaceAll("\n", "%0a");
+		r = r.replaceAll("#", "%23").replaceAll("?", "%3f").replaceAll("\\", "%5c").replaceAll("\r", "%0d").replaceAll("\n", "%0a");
 		return r;
+	};
+}
+
+// Reverses a string that was encoded with encodeURLPath
+if(!String.prototype.decodeURLPath)
+{
+	String.prototype.decodeURLPath = function decodeURLPath()
+	{
+		return this.replaceAll("%23", "#").replaceAll("%3f", "?").replaceAll("%5c", "\\").replaceAll("%0d", "\r").replaceAll("%0a", "\n").replaceAll("%20", " ").replaceAll("%25", "%");
 	};
 }
 
